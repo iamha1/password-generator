544899dde7e5f3fdb96d6dce991fa681be60489c
Ha Nguyen <nguyenthingocha2@gmail.com>
2020-02-07 20:01:47 -0500

    add content for README.md, clean up the script.js, connect with projects on my portfolio

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # password-generator
+* Define the variables for the characters that the password can contain (lowercase, uppercase letters, numbers and symbols characters).
+
+* Create the prompts for question "How mant characters would you like the password to contain?" and "Click OK including lowercase/uppercase/numbers/symbols"
+
+* Apply the conditonal statements for the lowercase, upppercase, numbers and symbols.
+
+* Apply the string split method to seperate the lowercase, uppercase, numbers, symbols characters.
+
+* Apply the Math.floor(Math.random) to create the possibility for the outcome of the password.
+
+* Run the password
+
+* Connect this assignment in the projects folder in my portfolio: https://iamha1.github.io/portfolio.html.
+
+* Reference: https://www.w3schools.com/ and https://stackoverflow.com/, and http://youTube.com/ 
--- script.js
+++ script.js
@@ -1,38 +1,56 @@
-// Assignment Code
-//var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-//}
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+var generateBtn = document.querySelector("#generate");
+var passText = document.querySelector("#password");
 
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase= "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-var numbers= "0123456789";
-var symbols= "!@#$%^&*()?";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()?";
 
 
-var lowercase= confirm("Click OK to confirm including lowercase characters.");
-var uppercase= confirm("Click OK to confirm including uppercase characters.");
-var numbers= confirm("Click OK to confirm including numbers.");
-var symbols= confirm("Click OK to confirm including special characters.");
+function myButton() {
+  var passwordLength = parseInt(prompt("How many characters would you like your password to contain?"));
 
-function myButton(){
-  var passwordLength = prompt("How many characters would you like your password to contain?");
-  console.log(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password does not meet the length requirements");
+  } else {
+    console.log(passwordLength);
+    var possibleCharacters = "";
 
+    var lowercaseConfirm = confirm("Click OK to confirm including lowercase characters.");
+
+    if (lowercaseConfirm == true) {
+      possibleCharacters += lowercase
+    }
+    var uppercaseConfirm = confirm("Click OK to confirm including uppercase characters.");
+
+    if (uppercaseConfirm == true) {
+      possibleCharacters += uppercase
+    }
+    var numbersConfirm = confirm("Click OK to confirm including numbers.");
+
+    if (numbersConfirm == true) {
+      possibleCharacters += numbers
+    }
+    var symbolsConfirm = confirm("Click OK to confirm including special characters.");
+    if (symbolsConfirm == true) {
+      possibleCharacters += symbols
+    }
+
+    possibleCharacters = possibleCharacters.split("");
+    // console.log(possibleCharacters);
+
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+      var character = possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
+      //console.log(character);
+      password += character;
+    }
+
+    console.log(passText)
+    console.log(password)
+    passText.value = password;
+  }
 }
-
-var possibleCharacters = (lowercase + uppercase + numbers + symbols ).split("");
-console.log(possibleCharacters);
-
-
-for (var passwordLength = 0; passwordLength >= 10 && passwordLength < 128; passwordLength++) {
-var character = Math.floor(Math.random() * possibleCharacters.length);
-password = all.substring(character, character + 1);
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", myButton)
